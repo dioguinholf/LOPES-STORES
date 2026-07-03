db74d0f42721a88844989bebaf8fb3fa49cf554e
Diogo Lopes de França <dl568585@gmail.com>
2026-07-03 05:27:10 +0000

    fix: adiciona prototipos das funcoes em routes.h

--- camisas-api/include/routes.h
+++ camisas-api/include/routes.h
@@ -2,7 +2,15 @@
 #define ROUTES_H
 
 #include "mongoose.h"
+#include "db.h"
 
 void handle_request(struct mg_connection *c, int ev, void *ev_data);
+
+void handler_times_listar(struct mg_connection *c, struct mg_http_message *hm, MYSQL *db);
+void handler_camisas_listar(struct mg_connection *c, struct mg_http_message *hm, MYSQL *db);
+void handler_camisa_obter(struct mg_connection *c, struct mg_http_message *hm, MYSQL *db, int id);
+void handler_camisa_criar(struct mg_connection *c, struct mg_http_message *hm, MYSQL *db);
+void handler_pedido_criar(struct mg_connection *c, struct mg_http_message *hm, MYSQL *db);
+void handler_pedido_obter(struct mg_connection *c, struct mg_http_message *hm, MYSQL *db, int id);
 
 #endif
